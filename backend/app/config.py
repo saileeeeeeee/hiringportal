1efd91aa9abbf10fff16c824d3e652844fd3dc4e
Sailesh S. <sailesh.s@ubtiinc.com>
2025-10-31 01:37:50 +0530

    Initial commit

---
+++ backend/app/config.py
@@ -0,0 +1,18 @@
+# app/config.py
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "UBTI Hiring Portal"
+    
+    # SQL Server config
+    DB_SERVER: str
+    DB_PORT: int = 1433
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+settings = Settings()
